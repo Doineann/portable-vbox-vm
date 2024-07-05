8f0a6aa415dfb5f3002db6f1dd3bdcc9591924cb
Doineann <doine4nn@gmail.com>
2024-07-05 20:56:02 +0200

    Fix for check if virtual harddisk needs detaching and made device number explicit

--- Scripts/HarddiskAttachImmutable.cmd
+++ Scripts/HarddiskAttachImmutable.cmd
@@ -14,4 +14,4 @@ Call HarddiskDetach.cmd
 echo - Set read-only attribute for harddisk image file
 attrib +R "%VM_DIR%\%VM_HDD_FILE%"
 echo - Attach harddisk as Immutable
-"%VBOX_INSTALL_DIR%\VBoxManage.exe" storageattach "%VM_VBOX_UUID%" --storagectl SATA --port 0 --type hdd --medium "%VM_DIR%\%VM_HDD_FILE%" --mtype immutable --nonrotational on --discard on
+"%VBOX_INSTALL_DIR%\VBoxManage.exe" storageattach "%VM_VBOX_UUID%" --storagectl SATA --port 0 --device 0 --type hdd --medium "%VM_DIR%\%VM_HDD_FILE%" --mtype immutable --nonrotational on --discard on
--- Scripts/HarddiskAttachMutable.cmd
+++ Scripts/HarddiskAttachMutable.cmd
@@ -14,4 +14,4 @@ Call HarddiskDetach.cmd
 echo - Remove read-only attribute for harddisk image file
 attrib -R "%VM_DIR%\%VM_HDD_FILE%"
 echo - Attach harddisk as Mutable
-"%VBOX_INSTALL_DIR%\VBoxManage.exe" storageattach "%VM_VBOX_UUID%" --storagectl SATA --port 0 --type hdd --medium "%VM_DIR%\%VM_HDD_FILE%" --mtype normal --nonrotational on --discard on
+"%VBOX_INSTALL_DIR%\VBoxManage.exe" storageattach "%VM_VBOX_UUID%" --storagectl SATA --port 0 --device 0 --type hdd --medium "%VM_DIR%\%VM_HDD_FILE%" --mtype normal --nonrotational on --discard on
